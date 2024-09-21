0262bec23f670bb9b84a720b7710285f7edb88ad
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-21 12:52:09 +0600

    updated db config

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -3,14 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+let isConnected: boolean = false;
+
 // Import MongoDB uri from the .env file
 const mongoURI = process.env.MONGO_CONNECTION_STRING as string;
 
 // Connect to MongoDB using Mongoose
 export const connectDB = async () => {
+	if (isConnected) {
+		console.log('⚡️ Using Existing MongoDB Connection!');
+		return;
+	}
+
 	try {
 		await mongoose.connect(mongoURI);
-		console.log('✅ DB is Connected!');
+		isConnected = true;
+		console.log('✅ MongoDB is Connected!');
 	} catch (error) {
 		if (error instanceof Error) {
 			console.error(error.message);
@@ -21,4 +29,3 @@ export const connectDB = async () => {
 		// process.exit(1);
 	}
 };
-
