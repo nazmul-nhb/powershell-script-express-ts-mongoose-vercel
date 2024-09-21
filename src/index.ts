0a397e96febf93cbbb0978407796c57a9cc02dfb
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-21 12:32:58 +0600

    restructured the index file

--- src/index.ts
+++ src/index.ts
@@ -11,55 +11,48 @@ const app: Application = express();
 const port = process.env.PORT || 4242;
 
 // Use IIFE to start the Server
-(async () => {
+
+// Middlewares
+// TODO: Add CORS Options when project is done!
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).send({ success: true, message: '🏃 Server is Running!' });
+});
+
+// Actual Routes
+app.use('/example', exampleRoutes);
+
+// Error Handler for 404
+app.use((req: Request, res: Response, next: NextFunction) => {
+	const error: IErrorObject = new Error('Requested URL Not Found!');
+	error.status = 404;
+	next(error);
+});
+
+// Final Error Handler
+app.use(
+	(error: IErrorObject, req: Request, res: Response, next: NextFunction) => {
+		console.error(error);
+		res.status(error.status || 500).send({
+			success: false,
+			message: error.message || 'Internal Server Error!',
+		});
+	},
+);
+
+// Start the Server
+app.listen(port, async () => {
 	try {
 		// Connect to DB
 		await connectDB();
-
-		// Middlewares
-		// TODO: Add CORS Options when project is done!
-		app.use(cors());
-		app.use(express.json());
-
-		// Routes
-		app.get('/', (req: Request, res: Response) => {
-			res.status(200).send({ success: true, message: '🏃 Server is Running!' });
-		});
-
-		// Actual Routes
-		app.use('/example', exampleRoutes);
-
-		// Error Handler for 404
-		app.use((req: Request, res: Response, next: NextFunction) => {
-			const error: IErrorObject = new Error('Requested URL Not Found!');
-			error.status = 404;
-			next(error);
-		});
-
-		// Final Error Handler
-		app.use(
-			(
-				error: IErrorObject,
-				req: Request,
-				res: Response,
-				next: NextFunction,
-			) => {
-				console.error(error);
-				res.status(error.status || 500).send({
-					success: false,
-					message: error.message || 'Internal Server Error!',
-				});
-			},
-		);
-
-		// Start the Server
-		app.listen(port, () => {
-			console.log('🏃 Server is Running on Port: ', port);
-		});
+		console.log('🏃 Server is Running on Port: ', port);
 	} catch (error) {
 		console.error('⚠️ Failed to Start the Server: ', error);
 		// process.exit(1);
 	}
-})();
+});
 
 export default app;
