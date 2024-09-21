84511ca181ea2df907b0339b4cc3d02a743a9c4d
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-21 11:47:51 +0600

    uninstalled jwt and related fumctions

--- src/routes/authRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, { Router } from 'express';
-import { sendToken } from '../controllers/authControllers';
-
-const router: Router = express.Router();
-
-router.post('/', sendToken);
-
-export default router;
-
